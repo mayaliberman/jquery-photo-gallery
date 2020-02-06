2894c5c5d0195699b264669a553787c93f5266ae
unknown <mayaliberman1@gmail.com>
2020-02-06 22:44:26 +0200

    adding search functionality

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ lightbox.option({
   wrapAround: true,
   alwaysShowNavOnTouchDevices: true,
   disableScrolling: true,
-  imageFadeDuration: 500, 
+  imageFadeDuration: 500,
   showImageNumberLabel: false
+});
+
+$(".input-box").on("keyup", function(event) {
+  let value = $(event.target)
+    .val()
+    .toLowerCase();
+
+  $("a[data-title]").each(function() {
+    var testdata = $(this).data("title").toLowerCase();
+    if (testdata.includes(value)) {
+      $(this).css({"display":"inline-block", "padding": "15px"});
+    } else {
+      $(this).css("display","none");
+      $(this).parent().css("padding", "0px")
+    }
+  });
 });
