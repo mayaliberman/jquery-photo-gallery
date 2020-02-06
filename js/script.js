86f129e30e51bdd65f77fb6b00a220a4b1f4d290
unknown <mayaliberman1@gmail.com>
2020-02-06 11:49:38 +0200

    configuring lightbox and styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("hello script");
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true,
+  alwaysShowNavOnTouchDevices: true,
+  disableScrolling: true,
+  imageFadeDuration: 500, 
+  showImageNumberLabel: false
+});
