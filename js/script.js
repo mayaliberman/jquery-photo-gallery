8004b7fabacfd0a936370c6e7e8a1bb85be71a40
unknown <mayaliberman1@gmail.com>
2020-02-07 10:45:41 +0200

    adding search event

--- js/script.js
+++ js/script.js
@@ -13,15 +13,32 @@ $(".input-box").on("keyup", function(event) {
     .toLowerCase();
 
   $("a[data-title]").each(function() {
-    let testdata = $(this).data("title").toLowerCase();
-    if(value === '') {
-        $(this).parent().removeClass('hide');
-        return;
+    let testdata = $(this)
+      .data("title")
+      .toLowerCase();
+    if (value === "") {
+      $(this)
+        .parent()
+        .removeClass("hide");
+      return;
     }
-   if(testdata.includes(value) && value !== '') {
-       $(this).parent().removeClass('hide');
-   } else {
-       $(this).parent().addClass('hide');
-   }
+    if (testdata.includes(value) && value !== "") {
+      $(this)
+        .parent()
+        .removeClass("hide");
+    } else {
+      $(this)
+        .parent()
+        .addClass("hide");
+    }
+  });
+});
+
+$(".input-box").on("search", function(event) {
+  $("a[data-title]").each(function() {
+    $(this)
+      .parent()
+      .removeClass("hide");
+       return;
   });
 });
